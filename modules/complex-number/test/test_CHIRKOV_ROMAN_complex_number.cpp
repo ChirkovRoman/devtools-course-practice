5be8d12f2ab4f4ae15570837f12efda4e35322b2
ChirkovRoman <romul14603@list.ru>
2021-03-29 23:20:32 +0300

    Added new tests for lab1

--- modules/complex-number/test/test_CHIRKOV_ROMAN_complex_number.cpp
+++ modules/complex-number/test/test_CHIRKOV_ROMAN_complex_number.cpp
@@ -4,9 +4,22 @@
 
 #include "include/complex_number.h"
 
-TEST(Chirkov_Roman_ComplexNumberTest, ){
+TEST(Chirkov_Roman_ComplexNumberTest, divide_by_zero) {
   ComplexNumber z1(123, 321);
   ComplexNumber z2(0.0, 0.0);
 
-  ASSERT_ANY_THROW(z1/z2);
+  ASSERT_ANY_THROW(z1 / z2);
+}
+
+TEST(Chirkov_Roman_ComplexNumberTest, multiplication_then_division) {
+  ComplexNumber z1(100, -100.1);
+  ComplexNumber z2(-10, -123.456);
+
+  ComplexNumber multExp(-13357.9456, -11344.6);
+  ComplexNumber multRes = z1 * z2;
+
+  ASSERT_EQ(multExp, multRes);
+
+  ComplexNumber divRes = multRes / z2;
+  ASSERT_EQ(divRes, z1);
 }
